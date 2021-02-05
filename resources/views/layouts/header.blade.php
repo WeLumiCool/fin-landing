256b2078f30ffa24b8a0993a643e77bffafc0706
Erkin <mackinkenny@gmail.com>
2021-02-05 12:25:27 +0600

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -20,9 +20,9 @@ $agent = new Agent();
                 <span class="{{ $agent->isDesktop() ? 'fin-menu-point' : 'fin-menu-mobile'}}">{{ __('main.about_us') }}</span>
                 </a>
 
-                <a href="">
-                <span class="{{ $agent->isDesktop() ? 'fin-menu-point' : 'fin-menu-mobile'}}">партнеры</span>
-                </a>
+                {{--<a href="">--}}
+                {{--<span class="{{ $agent->isDesktop() ? 'fin-menu-point' : 'fin-menu-mobile'}}">{{ _ }}</span>--}}
+                {{--</a>--}}
 
                 <a href="">
                 <span class="{{ $agent->isDesktop() ? 'fin-menu-point' : 'fin-menu-mobile'}}">{{ __('main.review') }}</span>
