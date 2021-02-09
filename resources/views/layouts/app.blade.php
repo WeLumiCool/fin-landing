69d3a820f3d7f2f7006050c3a6de6bfd9a7996a7
Frogy <nurs.axe@gmail.com>
2021-02-09 10:52:32 +0600

    edit text add icon title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,11 +7,11 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ __('main.consumer_credit') }}</title>
 
     <!-- Scripts -->
 
-
+    <link rel="icon" type="image/png" href="{{ asset('image/icons/fav.webp') }}">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Rubik:wght@400;500&display=swap" rel="stylesheet">
@@ -23,17 +23,17 @@
 </head>
 <body>
 @include('layouts.header')
-    <div id="app">
-        <main>
-            @yield('content')
-        </main>
-    </div>
+<div id="app">
+    <main>
+        @yield('content')
+    </main>
+</div>
 @include('layouts.footer')
 </body>
 <script src="{{ asset('js/app.js') }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/js/all.min.js" integrity="sha512-YSdqvJoZr83hj76AIVdOcvLWYMWzy6sJyIMic2aQz5kh2bPTd9dzY3NtdeEAzPp/PhgZqr4aJObB3ym/vsItMg==" crossorigin="anonymous"></script>
 
-    <script>
+<script>
     $(document).ready(function() {
         $(window).scroll(function() {
             var scrollTop = $(window).scrollTop();
