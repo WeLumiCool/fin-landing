da86a7a96b067697aac664bd5cfbc58dce2b459a
Lumaza Damir <damir290598@gmail.com>
2021-03-30 00:25:39 +0600

    add google tag manager and make unique landing

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,72 +1,72 @@
 <footer>
     <div style="background: rgb(36, 36, 36);">
-        <div class="" style="border-bottom: 1px solid #fefefe;">
-            <div class="container">
-                <div class="row py-3">
-                    <div class="col-lg-3 col-12">
-                        <a class="text-decoration-none" href="https://fin.kg/contacts#/">
-                            <h3 class="font-weight-normal font-size-16 mb-4" style="color: #DCDCDC;">
+        <div class="depozit " style="border-bottom: 1px solid #fefefe;">
+            <div class="depozit container">
+                <div class="depozit row py-3">
+                    <div class="depozit col-lg-3 col-12">
+                        <a class="depozit text-decoration-none" href="https://fin.kg/contacts#/">
+                            <h3 class="depozit font-weight-normal font-size-16 mb-4" style="color: #DCDCDC;">
                                 {{ __('main.help') }}
                             </h3>
 
                         </a>
-                        <a class="text-decoration-none" href="https://fin.kg/about#/">
-                            <h3 class="font-weight-normal font-size-16 mb-3" style="color: #DCDCDC;">
+                        <a class="depozit text-decoration-none" href="https://fin.kg/about#/">
+                            <h3 class="depozit font-weight-normal font-size-16 mb-3" style="color: #DCDCDC;">
                                 {{ __('main.about_project') }}
                             </h3>
                         </a>
                     </div>
-                    <div class="col-lg-3 col-12 mt-lg-0 mt-3">
-                        <h3 class="font-weight-normal font-size-16 mb-3" style="color: #DCDCDC;">
+                    <div class="depozit col-lg-3 col-12 mt-lg-0 mt-3">
+                        <h3 class="depozit font-weight-normal font-size-16 mb-3" style="color: #DCDCDC;">
                             {{ __('main.commercial_department') }}
                         </h3>
-                        <p class="font-size-12 font-weight-light mb-1" style="color: #DCDCDC;">
-                            <a class="text-decoration-none" style="color: #DCDCDC;" href="tel:+996990120163">
+                        <p class="depozit font-size-12 font-weight-light mb-1" style="color: #DCDCDC;">
+                            <a class="depozit text-decoration-none" style="color: #DCDCDC;" href="tel:+996990120163">
                                 +996 990 12 01 63
                             </a>
                         </p>
-                        <p class="font-size-12 font-weight-light mb-4" style="color: #DCDCDC;">
-                            <a class="text-decoration-none" style="color: #DCDCDC;" href="mailto:sales@fin.kg">
+                        <p class="depozit font-size-12 font-weight-light mb-4" style="color: #DCDCDC;">
+                            <a class="depozit text-decoration-none" style="color: #DCDCDC;" href="mailto:sales@fin.kg">
                                 sales@fin.kg
                             </a>
                         </p>
                     </div>
-                    <div class="col-lg-3 col-12 mt-lg-0 mt-3">
-                        <h3 class="font-weight-normal font-size-16 mb-3" style="color: #DCDCDC;">
+                    <div class="depozit col-lg-3 col-12 mt-lg-0 mt-3">
+                        <h3 class="depozit font-weight-normal font-size-16 mb-3" style="color: #DCDCDC;">
                             {{ __('main.advertisement') }}
                         </h3>
-                        <p class="font-size-12 font-weight-light mb-1" style="color: #DCDCDC;">
-                            <a class="text-decoration-none" style="color: #DCDCDC;" href="tel:+996550704630">
+                        <p class="depozit font-size-12 font-weight-light mb-1" style="color: #DCDCDC;">
+                            <a class="depozit text-decoration-none" style="color: #DCDCDC;" href="tel:+996550704630">
                                 +996 550 70 46 30
                             </a>
                         </p>
-                        <p class="font-size-12 font-weight-light mb-4" style="color: #DCDCDC;">
-                            <a class="text-decoration-none" style="color: #DCDCDC;" href="mailto:partners@fin.kg">
+                        <p class="depozit font-size-12 font-weight-light mb-4" style="color: #DCDCDC;">
+                            <a class="depozit text-decoration-none" style="color: #DCDCDC;" href="mailto:partners@fin.kg">
                                 partners@fin.kg
                             </a>
                         </p>
                     </div>
-                    <div class="col-lg-3 col-12 mt-lg-0 mt-3">
-                        <h3 class="font-weight-normal font-size-16 mb-3" style="color: #DCDCDC;">
+                    <div class="depozit col-lg-3 col-12 mt-lg-0 mt-3">
+                        <h3 class="depozit font-weight-normal font-size-16 mb-3" style="color: #DCDCDC;">
                             {{ __('main.contacts') }}
                         </h3>
-                        <p class="font-size-12 font-weight-light mb-1" style="color: #DCDCDC;">
-                            <a class="text-decoration-none" style="color: #DCDCDC;" href="tel:+996777502233">
+                        <p class="depozit font-size-12 font-weight-light mb-1" style="color: #DCDCDC;">
+                            <a class="depozit text-decoration-none" style="color: #DCDCDC;" href="tel:+996777502233">
                                 +996 777 50 22 33
                             </a>
                         </p>
-                        <p class="font-size-12 font-weight-light mb-1" style="color: #DCDCDC;">
-                            <a class="text-decoration-none" style="color: #DCDCDC;" href="tel:+996705502233">
+                        <p class="depozit font-size-12 font-weight-light mb-1" style="color: #DCDCDC;">
+                            <a class="depozit text-decoration-none" style="color: #DCDCDC;" href="tel:+996705502233">
                                 +996 705 50 22 33
                             </a>
                         </p>
-                        <p class="font-size-12 font-weight-light mb-1" style="color: #DCDCDC;">
-                            <a class="text-decoration-none" style="color: #DCDCDC;" href="tel:+996553502233">
+                        <p class="depozit font-size-12 font-weight-light mb-1" style="color: #DCDCDC;">
+                            <a class="depozit text-decoration-none" style="color: #DCDCDC;" href="tel:+996553502233">
                                 +996 553 50 22 33
                             </a>
                         </p>
-                        <p class="font-size-12 font-weight-light mb-1" style="color: #DCDCDC;">
-                            <a class="text-decoration-none" style="color: #DCDCDC;" href="mailto:info@fin.kg">
+                        <p class="depozit font-size-12 font-weight-light mb-1" style="color: #DCDCDC;">
+                            <a class="depozit text-decoration-none" style="color: #DCDCDC;" href="mailto:info@fin.kg">
                                 info@fin.kg
                             </a>
                         </p>
@@ -75,34 +75,34 @@
             </div>
         </div>
         <div style="border-bottom: 1px solid #606D99;">
-            <div class="container">
-                <div class="row py-3 align-items-end">
-                    <div class="col-lg-3 col-6">
-                        <p class="small font-size-12 mb-0" style="color: #ffffff;">
+            <div class="depozit container">
+                <div class="depozit row py-3 align-items-end">
+                    <div class="depozit col-lg-3 col-6">
+                        <p class="depozit small font-size-12 mb-0" style="color: #ffffff;">
                             © 2020 — 2021 ОсОО "Фин Маркет Плюс"
                         </p>
                     </div>
-                    <div class="col-lg-3 col-6">
+                    <div class="depozit col-lg-3 col-6">
                         <a href="https://fin.kg/terms_of_use#/">
-                            <p class="small font-size-12 mb-0" style="color: #ffffff;">
+                            <p class="depozit small font-size-12 mb-0" style="color: #ffffff;">
                                 {{ __('main.policity') }}
                             </p>
                         </a>
                     </div>
-                    <div class="col-lg-3 col-6">
+                    <div class="depozit col-lg-3 col-6">
                         <a href="https://fin.kg/privacy_policy#/">
-                            <p class="small font-size-12 mb-0" style="color: #ffffff;">
+                            <p class="depozit small font-size-12 mb-0" style="color: #ffffff;">
                                 {{ __('main.privacy_policy') }}
                             </p>
                         </a>
                     </div>
-                    <div class="col-lg-3 col-6">
+                    <div class="depozit col-lg-3 col-6">
                         @if(app()->getLocale()=='ru')
-                            <p class="small font-size-10 mb-0 text-white">При использовании материалов гиперссылка на
-                                <span class="text-info">Fin.kg</span> обязательна</p>
+                            <p class="depozit small font-size-10 mb-0 text-white">При использовании материалов гиперссылка на
+                                <span class="depozit text-info">Fin.kg</span> обязательна</p>
                         @else
-                            <p class="small font-size-10 mb-0 text-white">Материалдарды колдонууда <span
-                                        class="text-info">Fin.kgге</span> гипершилтеме талап кылынат</p>
+                            <p class="depozit small font-size-10 mb-0 text-white">Материалдарды колдонууда <span
+                                        class="depozit text-info">Fin.kgге</span> гипершилтеме талап кылынат</p>
                         @endif
                     </div>
                 </div>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,41 +3,36 @@ use Jenssegers\Agent\Agent;
 
 $agent = new Agent();
 ?>
-<nav class="navbar navbar-expand-md navbar-light shadow-sm menuse shadow-none py-3 bg-white" style="position: sticky; top:0%; z-index: 99999;">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            <img class="img-fluid" src="{{ asset('image/icons/logo.svg') }}" alt="">
+<nav class="depozit navbar navbar-expand-md navbar-light shadow-sm menuse shadow-none py-3 bg-white" style="position: sticky; top:0%; z-index: 99999;">
+    <div class="depozit container">
+        <a class="depozit navbar-brand" href="{{ url('/') }}">
+            <img class="depozit img-fluid" src="{{ asset('image/icons/logo.svg') }}" alt="">
         </a>
-        <button class="navbar-toggler border-0 font-size-20" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
+        <button class="depozit navbar-toggler border-0 font-size-20" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="depozit navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="depozit collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Right Side Of Navbar -->
-            <div class="navbar-nav mx-auto d-flex align-items-center" id="pick">
+            <div class="depozit navbar-nav mx-auto d-flex align-items-center" id="pick">
                 <!-- Authentication Links -->
-                <a href="#about">
-                    <span class="{{ $agent->isDesktop() ? 'fin-menu-point' : 'fin-menu-mobile'}}">{{ __('main.about_us') }}</span>
+                <a href="#depozit-about">
+                    <span class="depozit {{ $agent->isDesktop() ? 'fin-menu-point' : 'fin-menu-mobile'}}">{{ __('main.about_us') }}</span>
                 </a>
-
-                {{--<a href="">--}}
-                {{--<span class="{{ $agent->isDesktop() ? 'fin-menu-point' : 'fin-menu-mobile'}}">{{ _ }}</span>--}}
-                {{--</a>--}}
-
-                <a href="#feedback">
-                <span class="{{ $agent->isDesktop() ? 'fin-menu-point' : 'fin-menu-mobile'}}">{{ __('main.review') }}</span>
+                <a href="#depozit-feedback">
+                <span class="depozit {{ $agent->isDesktop() ? 'fin-menu-point' : 'fin-menu-mobile'}}">{{ __('main.review') }}</span>
                 </a>
 
 
 
             </div>
-            <div class="navbar-nav ml-auto d-flex align-items-center">
+            <div class="depozit navbar-nav ml-auto d-flex align-items-center">
                 <a class=
                    "{{ $agent->isDesktop() ? 'fin-menu-point' : 'fin-menu-mobile'}}" href="{{ route('language.switch', 'ru') }}">RU</a>
                 <a class=
                    "{{ $agent->isDesktop() ? 'fin-menu-point' : 'fin-menu-mobile'}} mr-lg-3 mr-0" href="{{ route('language.switch', 'kg') }}">KG</a>
                 <a href="http://fin.kg/type_list/9">
-                    <button class="btn fin-btn" style="padding: 10px 32px;">
+                    <button class="depozit btn fin-btn" style="padding: 10px 32px;">
                         {{__('main.master')}}
                     </button>
                 </a>
